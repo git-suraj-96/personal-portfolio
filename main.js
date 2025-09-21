3185fc4d74581daf587216831766045e14357b81
Self-Developer <git.suraj.96@gmail.com>
2025-09-21 21:57:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,25 @@ const btn = document.querySelector(".contact");
 btn.addEventListener("click", () =>{
     form.reset();
     alert("Thanks for contacting me");
-})
+
+});
+
+
+const spantag = document.querySelector("#animated-text");
+const text = "Frontend Developer";
+let index = 1;
+function print(){
+  if(index > text.length){
+    index = 1;
+  }
+  setTimeout(()=>{
+    spantag.innerHTML = text.slice(0, index);
+    index++;
+
+  }, 100);
+}
+;
+
+setInterval(() => {
+  print();
+},300);
